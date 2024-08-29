9824f6c5c846e699371204dcb4da3e0a99a1b381
gaspard <gaspard.johanet@gmail.com>
2024-08-29 13:13:04 +0200

    fix database controller

--- sample-project/back/controllers/controllerDatabase.js
+++ sample-project/back/controllers/controllerDatabase.js
@@ -8,10 +8,10 @@ require('dotenv').config()
 exports.AllTables = async (req, res) => {
     try {
         await sequelize.query('CREATE DATABASE IF NOT EXISTS ' + process.env.DBDATABASE);
-        await User.sync();
-        await File.sync();
-        await Subscription.sync();
-        await Invoice.sync();
+        await User.sync({ force: true });
+        await File.sync({ force: true });
+        await Subscription.sync({ force: true });
+        await Invoice.sync({ force: true });
         res.status(200).json('Toutes les tables sont synchronisées avec succès');
     } catch (error) {
         res.status(500).json(error);
