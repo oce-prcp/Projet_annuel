f31ba4e7f7a995c51924505825d4e026e0a467a6
carlbrgs <carlbrgs77@gmail.com>
2024-09-10 09:58:43 +0200

    Update ErrorPage.jsx to display correct error message and login button

--- sample-project/front/src/pages/ErrorPage.jsx
+++ sample-project/front/src/pages/ErrorPage.jsx
@@ -7,16 +7,16 @@ const ErrorPage = () => {
     return (
         <>
             <Container className="error-bloc text-center" style={{ padding: '50px 0' }}>
-                <h1 style={{ fontSize: '3rem', fontWeight: 'bold', color: '#038A8A' }}>Erreur 401</h1>
+                <h1 style={{ fontSize: '3rem', fontWeight: 'bold', color: '#038A8A' }}>Erreur</h1>
                 <p style={{ fontSize: '1.5rem', marginBottom: '20px' }}>
-                    Vous n'êtes pas autorisé à accéder à cette page.
+                    Vous n'êtes pas connecter pour accéder à cette page.
                 </p>
                 <Button 
                     variant="primary" 
-                    href="/" 
+                    href="/login" 
                     style={{ padding: '10px 20px', fontSize: '1.2rem', backgroundColor: '#038A8A', border: 'none' }}
                 >
-                    Retour à l'accueil
+                    Se connecter ou créer un compte
                 </Button>
             </Container>
         </>
